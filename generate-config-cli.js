d14b48aedc3de43c369d1db0480d6f701695cd99
Cody253 <brunomediadev@gmail.com>
2026-03-31 13:04:54 -0500

    who fucking knows

--- generate-config-cli.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-import('file://' + new URL('./generate-config.js', import.meta.url).pathname)
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
--- generate-conlang-cli.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-// Node-friendly CommonJS shim to load ES module
-import('file://' + new URL('./generate-conlang.js', import.meta.url).pathname)
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
