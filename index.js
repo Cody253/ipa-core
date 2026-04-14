e66fb7cfff1ef7b4e24e4abf5355863ef524a20d
Cody253 <brunomediadev@gmail.com>
2026-04-14 15:06:47 -0500

    fix function exports

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const modifiers = require("./modifiers.js"); // diacritics, tones, suprasegmenta
 const rules = require("./rules.js");         // modifier compatibility rules
 const parser = require("./parser.js");       // parser (parsePhoneme, parseUnit, parseConfig)
 const dictionary = require("./dictionary.js");      //instructional definitions
-const getDefinitions = require("./get-definitions.js");    //compiles and formats instructions
+const { getDefinitions } = require("./get-definitions.js");    //compiles and formats instructions
 
 // Export the layers so users can access them if needed
 const { parsePhoneme, parseUnit, parseConfig } = parser;
